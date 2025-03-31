f0496411824a87f0bd4b81e1a59a97fce67496bf
Richard DW Redcroft <Richard@Redcroft.tech>
2025-03-31 20:19:08 +0100

    Fix bootstrap

--- .chezmoiscripts/run_onchange_40-setup-zimfw.sh
+++ .chezmoiscripts/run_onchange_40-setup-zimfw.sh
@@ -7,11 +7,13 @@ setup_zimfw() {
         curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
             https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
     fi
+    if [[ -e ${HOME}.zimrc ]]; then
     # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
         if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
           source ${ZIM_HOME}/zimfw.zsh init -q
         fi
         source ${ZIM_HOME}/init.zsh
+    fi
 EOF
 }
 
